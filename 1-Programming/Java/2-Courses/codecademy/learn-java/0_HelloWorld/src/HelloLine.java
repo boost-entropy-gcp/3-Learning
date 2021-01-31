8bc7188a581795af471b40fcdb8d794501cf0a64
IvanNardini <ivan.nardini92@gmail.com>
2021-01-31 09:00:50 +0100

    delete files

--- 1-Programming/Java/2-Courses/codecademy/learn-java/0_HelloWorld/src/HelloLine.java
+++
@@ -1,7 +0,0 @@
-
-public class HelloLine {
-    public static void main(String[] args) {
-        System.out.print("Hello ");
-        System.out.print("Line");
-    }
-}
--- 1-Programming/Java/2-Courses/codecademy/learn-java/0_HelloWorld/src/LanguageFacts.java
+++
@@ -1,9 +0,0 @@
-public class LanguageFacts {
-    public static void  main(String[] args){
-        // java does not interpret whitespaces
-        // It does semicolons ;
-        System.out.println("Java is a class-based language.");
-        System.out.println("Java classes have a 'main' method.");
-        System.out.println("Java statements end with a semicolon.");
-    }
-}
