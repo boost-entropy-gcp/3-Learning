56cdfef94bbf78ef1279dcc48b7bb394a4684f7b
IvanNardini <ivan.nardini92@gmail.com>
2021-02-19 08:52:53 +0100

    inheritance and polymorphism module done

--- Programming/Java/2-Courses/codecademy/learn-java/12_InheritancePolymorphism/InheritancePolymorphism/src/Noodle.java
+++ Programming/Java/2-Courses/codecademy/learn-java/12_InheritancePolymorphism/InheritancePolymorphism/src/Noodle.java
@@ -1,15 +1,50 @@
 public class Noodle {
 
-    double lenghtInCentimeters;
-    String shape;
-    String texture = "brittle";
+    protected double lenghtInCentimeters;
+    protected double widthCentimeters;
+    protected String shape;
+    protected String ingredients;
+    protected String texture = "brittle";
+
+    public Noodle(double lenghtInCentimeters, double widthCentimeters, String shape, String ingredients) {
+        this.lenghtInCentimeters = lenghtInCentimeters;
+        this.widthCentimeters = widthCentimeters;
+        this.shape = shape;
+        this.ingredients = ingredients;
+    }
 
     public void cook(){
+        System.out.println("Boiling.");
         this.texture = "cooked";
     }
 
-    public static void main(String[] args){
-        Spaghetti spaghettiPomodoro = new Spaghetti();
-        System.out.println(spaghettiPomodoro.texture);
+    public String getCookPrep(){
+        return "Boil noodle for 7 minutes and add sauce.";
+    }
+
+    final public boolean isTasty(){
+        return true;
+    }
+
+//    public static void main(String[] args){
+////        Spaghetti spaghettiPomodoro = new Spaghetti(20, 20, "long", "soup");
+////        System.out.println(spaghettiPomodoro.texture);
+////        Pho pho = new Pho();
+////        System.out.println(pho.shape);
+////        Ramen yasaiRamen = new Ramen();
+////        System.out.println(yasaiRamen.isTasty());
+////        System.out.println(yasaiRamen.ingredients);
+//        Spaetzle kaesespaetzle = new Spaetzle();
+//        kaesespaetzle.cook();
+//    }
+    public static void main(String[] args) {
+        Noodle spaghetti, ramen, pho; //declare
+        spaghetti = new Spaghetti();
+        ramen = new Ramen();
+        pho = new Pho();
+        Noodle[] allTheNoodles = {spaghetti, ramen, pho};
+        for(Noodle noodle : allTheNoodles){
+            System.out.println(noodle.getCookPrep());
+        }
     }
 }
--- Programming/Java/2-Courses/codecademy/learn-java/12_InheritancePolymorphism/InheritancePolymorphism/src/Spaghetti.java
+++ Programming/Java/2-Courses/codecademy/learn-java/12_InheritancePolymorphism/InheritancePolymorphism/src/Spaghetti.java
@@ -1,6 +1,11 @@
-//public class Spaghetti {
-//}
-
 class Spaghetti extends Noodle{
+    public Spaghetti() {
+        super(30.0, 0.2, "round", "semolina flour");
+    }
+    @Override
+    public String getCookPrep() {
 
+        return "Boil spaghetti for 8 - 12 minutes and add sauce, cheese, or oil and garlic.";
+
+    }
 }
