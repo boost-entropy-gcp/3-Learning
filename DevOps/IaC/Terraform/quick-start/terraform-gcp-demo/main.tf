08fa0a3a88587f55a7ff25b957037ccc3bcd1d89
IvanNardini <ivan.nardini92@gmail.com>
2021-03-24 06:57:06 +0100

    vpc and compute instance tested

---
+++ DevOps/IaC/Terraform/quick-start/terraform-gcp-demo/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+
+  credentials = file("inardini-playground-a2c42af9891c.json")
+  project = "inardini-playground"
+  region  = "europe-west1"
+  zone    = "europe-west1-b"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  tags         = ["web", "dev"]
+
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
+
