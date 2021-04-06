2c0ab21b818b31b1f6e0164e8e299e32cf932962
IvanNardini <ivan.nardini92@gmail.com>
2021-04-06 08:11:52 +0200

    image uploader base done.

---
+++ FrontEnd/2-Courses/Coursera/html_css_js_foundation/interactive/image_uploader_base/README.markdown
@@ -0,0 +1,5 @@
+# image_uploader_base
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/ivannardini/pen/vYgJGZo](https://codepen.io/ivannardini/pen/vYgJGZo).
+
+
---
+++ FrontEnd/2-Courses/Coursera/html_css_js_foundation/interactive/image_uploader_base/dist/script.js
@@ -0,0 +1,11 @@
+function upload() {
+  
+ // Create a variable that gets the value of the text from the text input element
+  var fileinput = document.getElementById("inputText")
+  var filename = fileinput.value;
+  
+ //Display this text in an alert.
+  alert("You chose " + filename);
+ 
+  
+}
